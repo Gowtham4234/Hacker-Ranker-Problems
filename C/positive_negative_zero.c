21204b66d852364dc4e2b04bae1a2528cfef907a
Gowtham4234 <ceitgowtham27@gmail.com>
2025-01-31 21:42:48 +0530

    feat : Positive negative zero

---
+++ C/positive_negative_zero.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+
+int main() {
+    int arr_count = 5;
+    int arr[] = {1, 1, 0, -1, -1};
+    float positive = 0, negative = 0, zero = 0;
+
+    for (int i = 0; i < arr_count; i++) {
+        if (arr[i] > 0) {
+            positive += 1;
+        } else if (arr[i] < 0) {
+            negative += 1;
+        } else {
+            zero += 1;
+        }
+    }
+    double p = positive / arr_count;
+    double n = negative / arr_count;
+    double z = zero / arr_count;
+
+
+    printf("%.6lf\n", p); 
+    printf("%.6lf\n", n); 
+    printf("%.6lf\n", z); 
+    return 0;
+}
