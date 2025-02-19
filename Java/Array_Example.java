0c62bffab4b82f8addd1ea66d11a4012896d180c
Gowtham4234 <ceitgowtham27@gmail.com>
2025-02-19 20:59:39 +0530

    array

---
+++ Java/Array_Example.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class Array_Example {
+    public static void main(String[] args) {
+        
+        int []arr = {1,2,3,4,5,6,7,8,9,10};
+        int  mat[] = {10,9,8,7,6,5,4,3,2,1};
+        
+        System.out.println(arr[2]);
+        System.out.println(mat[1]);
+
+
+        for(int i=0;i<mat.length;i++){
+            System.out.print(mat[i]);
+        }
+
+    }
+}
